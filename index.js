1894ff480f95e317c80b1d186ca9b1277a49af73
Andrei Cacio <andrei.cacio@yahoo.com>
2016-07-14 16:02:28 +0300

    Removed the core file and moved it into the main index.js file

---
+++ index.js
@@ -0,0 +1,5 @@
+import Immutable from 'immutable';
+import createStore from './src/store';
+import dispatcher from './src/dispatcher';
+
+export default { Immutable, createStore, dispatcher };
--- src/core.js
+++
@@ -1,5 +0,0 @@
-import Immutable from 'immutable';
-import createStore from './store';
-import dispatcher from './dispatcher';
-
-export default { Immutable, createStore, dispatcher };
