b896b309a0af1bcb249629b18552ec63c75c7de7
Andrei Cacio <andrei.cacio@yahoo.com>
2016-07-17 17:02:01 +0300

    Added object spread plugin

--- src/rxflux/store-creator.js
+++ src/rxflux/store-creator.js
@@ -1,28 +1,28 @@
 import { BehaviorSubject } from 'rxjs';
 import Immutable from 'immutable';
 import dispatcher from './dispatcher';
-import uuid from 'uuid';
 
 let globalState = Immutable.fromJS({
   stores: {},
   state: {}
 });
 
-export default function createStore(obj) {
-  const storeId = uuid.v4();
+export default function createStore(name, obj) {
   const initialState = Immutable.fromJS(obj.initialState);
   const stateSubject = new BehaviorSubject(initialState);
 
   globalState = globalState
-    .update('stores', stores => stores.set(storeId, obj))
-    .update('state', state => state.set(storeId, initialState));
+    .update('stores', stores => stores.set(name, obj))
+    .update('state', state => state.set(name, initialState));
 
   dispatcher.subscribe(actionDispatch => {
     const prevState = globalState.get('state').get(storeId);
 		if (obj.actionMap.hasOwnProperty(actionDispatch.action)) {
 			const newState = obj.actionMap[actionDispatch.action](prevState, actionDispatch.payload);
 
-      globalState = globalState.update('state', state => state.update(storeId, store => store.merge(newState)));
+      globalState = globalState
+        .update('state', state => state.update(storeId, store => store.merge(newState)));
+      
 	    stateSubject.next(newState);
 	  }
 	});
